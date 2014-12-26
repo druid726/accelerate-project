988c1898edc328c714e726d63ccba794e6fcde1f
druid726 <pdoneill26@gmail.com>
2014-12-26 14:55:37 -0500

    added security and anti-spam plugins and made sticky navbar

---
+++ wp-content/plugins/wordfence/tmp/configCache.php
@@ -0,0 +1,4 @@
+<?php
+/* Wordfence temporary file security header */
+echo "Nothing to see here!\n"; exit(0);
+?>a:19:{s:9:"cacheType";s:0:"";s:15:"firewallEnabled";s:1:"1";s:6:"apiKey";s:160:"89a42c454a8150c5d567eac28dbdb707bfafa2f9f6ba2bddfa1c46a96d4966130d01f29b91f5a31b2d1342313471f7fb12a652f1c8efbb0c43c381177b4e7f8684b0bb0aab3412477bdcd3c921a83cd4";s:9:"howGetIPs";s:0:"";s:11:"whitelisted";s:0:"";s:13:"cbl_countries";s:0:"";s:18:"cbl_bypassRedirURL";s:0:"";s:17:"cbl_bypassViewURL";s:0:"";s:11:"blockedTime";s:3:"300";s:18:"liveTrafficEnabled";s:1:"1";s:14:"disableCookies";s:1:"0";s:17:"actUpdateInterval";s:0:"";s:7:"debugOn";s:1:"0";s:10:"tourClosed";s:1:"1";s:25:"liveTraf_ignorePublishers";s:1:"1";s:19:"other_hideWPVersion";s:1:"1";s:10:"autoUpdate";s:1:"1";s:11:"alertEmails";s:20:"pdoneill26@gmail.com";s:24:"other_pwStrengthOnUpdate";s:1:"1";}
